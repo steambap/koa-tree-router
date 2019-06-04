7ef01563d7035d68befcd66545268e206e28d6ff
Weilin Shi <934587911@qq.com>
2019-06-04 12:43:04 +0800

    Update definition to support parameterized context

--- index.d.ts
+++ index.d.ts
@@ -1,84 +1,121 @@
 import * as Koa from "koa";
 
-declare module "koa" {
-  interface Context {
-    params: any;
-  }
-}
-
 declare namespace Router {
-  export interface IMiddleware {
-    (ctx: Router.IRouterContext, next: () => Promise<any>): any;
-  }
-  export interface IRouterOptions {
-    onMethodNotAllowed?: Router.IMiddleware;
+  export interface IRouterOptions<StateT = any, CustomT = {}> {
+    onMethodNotAllowed?: Router.Middleware<StateT, CustomT>;
     prefix?: string;
   }
-  export interface IRouterContext extends Koa.Context {
+  export interface IRouterParamContext<StateT = any, CustomT = {}> {
     /**
      * url params
      */
     params: { [key: string]: string };
+    /**
+     * the router instance
+     */
+    router: Router<StateT, CustomT>;
   }
+  export type RouterContext<
+    StateT = any,
+    CustomT = {}
+  > = Koa.ParameterizedContext<
+    StateT,
+    CustomT & IRouterParamContext<StateT, CustomT>
+  >;
+  export interface IRouterContext extends RouterContext {}
+  export type Middleware<StateT = any, CustomT = {}> = Koa.Middleware<
+    StateT,
+    CustomT & IRouterParamContext<StateT, CustomT>
+  >;
 }
 
-declare class Router {
+declare class Router<StateT = any, CustomT = {}> {
   /**
    * Create a new router.
    */
-  constructor(opts?: Router.IRouterOptions);
+  constructor(opts?: Router.IRouterOptions<StateT, CustomT>);
   /**
    * Register a new route.
    */
   on(
     method: string,
     path: string,
-    ...middleware: Array<Router.IMiddleware>
+    ...middleware: Array<Router.Middleware<StateT, CustomT>>
   ): this;
   /**
    * HTTP GET method
    */
-  get(path: string, ...middleware: Array<Router.IMiddleware>): this;
+  get(
+    path: string,
+    ...middleware: Array<Router.Middleware<StateT, CustomT>>
+  ): this;
   /**
    * HTTP POST method
    */
-  post(path: string, ...middleware: Array<Router.IMiddleware>): this;
+  post(
+    path: string,
+    ...middleware: Array<Router.Middleware<StateT, CustomT>>
+  ): this;
   /**
    * HTTP PUT method
    */
-  put(path: string, ...middleware: Array<Router.IMiddleware>): this;
+  put(
+    path: string,
+    ...middleware: Array<Router.Middleware<StateT, CustomT>>
+  ): this;
   /**
    * HTTP DELETE method
    */
-  delete(path: string, ...middleware: Array<Router.IMiddleware>): this;
+  delete(
+    path: string,
+    ...middleware: Array<Router.Middleware<StateT, CustomT>>
+  ): this;
   /**
    * HTTP HEAD method
    */
-  head(path: string, ...middleware: Array<Router.IMiddleware>): this;
+  head(
+    path: string,
+    ...middleware: Array<Router.Middleware<StateT, CustomT>>
+  ): this;
   /**
    * HTTP OPTIONS method
    */
-  options(path: string, ...middleware: Array<Router.IMiddleware>): this;
+  options(
+    path: string,
+    ...middleware: Array<Router.Middleware<StateT, CustomT>>
+  ): this;
   /**
    * HTTP PATCH method
    */
-  patch(path: string, ...middleware: Array<Router.IMiddleware>): this;
+  patch(
+    path: string,
+    ...middleware: Array<Router.Middleware<StateT, CustomT>>
+  ): this;
   /**
    * HTTP TRACE method
    */
-  trace(path: string, ...middleware: Array<Router.IMiddleware>): this;
+  trace(
+    path: string,
+    ...middleware: Array<Router.Middleware<StateT, CustomT>>
+  ): this;
   /**
    * HTTP CONNECT method
    */
-  connect(path: string, ...middleware: Array<Router.IMiddleware>): this;
+  connect(
+    path: string,
+    ...middleware: Array<Router.Middleware<StateT, CustomT>>
+  ): this;
   /**
    * Register route with all methods.
    */
-  all(path: string, ...middleware: Array<Router.IMiddleware>): this;
+  all(
+    path: string,
+    ...middleware: Array<Router.Middleware<StateT, CustomT>>
+  ): this;
   /**
    * Returns router middleware.
    */
-  routes(): Router.IMiddleware;
+  routes(): Router.Middleware<StateT, CustomT>;
 }
 
 export = Router;
